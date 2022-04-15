5e381b470184c1503cdcd336500ad6f50494b3b3
Alejandro Cruz <AlejandroCruz@users.noreply.github.com>
2022-04-15 17:40:07 -0400

    Add project files.

---
+++ HR.LeaveManagement.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Domain.Common
+{
+    // can be accessed by any in same assembly, but not from another assembly.
+    internal abstract class BaseDomainEntity
+    {
+        public DateTime CreatedDate { get; set; }
+        public DateTime DateLastModified { get; set; }
+        public int Id { get; set; }
+        public string CreatedBy { get; set; }
+        public string LastModifiedBy { get; set; }
+    }
+}
---
+++ HR.LeaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,17 @@
+﻿using HR.LeaveManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Domain
+{
+    internal class LeaveAllocation : BaseDomainEntity
+    {
+        public int LeaveTypeId { get; set; }
+        public int NumberOfDays { get; set; }
+        public int Period { get; set; }
+        public LeaveType LeaveType { get; }
+    }
+}
