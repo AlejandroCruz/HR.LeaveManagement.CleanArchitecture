a85efa04bfc1d7d6d2787905d02652492b48b39e
Alejandro Cruz <AlejandroCruz@users.noreply.github.com>
2022-04-16 16:42:02 -0400

    App startup service.

---
+++ HR.LeaveManagement.App/AppStartup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.App
+{
+    public static class AppStartup
+    {
+        public static void AppConfigureServices(this IServiceCollection services)
+        {
+            /* 'GetExecutingAssembly()' traverses all 'Profile' types for one generic
+                call for mapper vs. multiple [model]profiles */
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+    }
+}
